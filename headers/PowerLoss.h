ba5637550b00ff1e374e42963115c2a18fe2e79d
Prahtz <lorenzopratesi3@gmail.com>
2021-01-30 14:44:23 +0100

    Callback function

--- headers/PowerLoss.h
+++ headers/PowerLoss.h
@@ -4,13 +4,28 @@
 #else
     #include "RCSim.h"
     const int INT_EDGE_BOTH = 0;
+    static const int digitalRead(int pin) {return 0;}
     static const int wiringPiISR (int pin, int edgeType,  void (*function)(void)) {return 0;}
 #endif
 
 #include <atomic>
 #include <iostream>
+#include <chrono>
+#include <thread>
+
 
 static std::atomic<bool> call;
+static void callback() {
+    unsigned int sleep = 3000;
+    int r = digitalRead(22);
+    if(r == 0) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(sleep));
+        r = digitalRead(22);
+        if(r == 0) {
+            std::cout << "testing" << std::endl;
+        }
+    }
+}
 
 class PowerLoss {
     private:
@@ -18,5 +33,4 @@ class PowerLoss {
     public:
         PowerLoss(){};
         static void test();
-        void callback();
 };
--- source/PowerLoss.cpp
+++ source/PowerLoss.cpp
@@ -1,11 +1,6 @@
 #include "PowerLoss.h"
-
 void PowerLoss::test() {
     call = false;
     wiringPiSetup();
-    wiringPiISR(PIN, INT_EDGE_BOTH,[] () {std::cout<<"testing"<<std::endl;});
-}
-
-void PowerLoss::callback() {
-
+    wiringPiISR(PIN, INT_EDGE_BOTH, callback);
 }
