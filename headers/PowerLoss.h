87e043b8327c985071786e0314419818eda7398a
Prahtz <lorenzopratesi3@gmail.com>
2021-03-15 17:12:11 +0100

    Changed power loss detection's policy

--- headers/PowerLoss.h
+++ headers/PowerLoss.h
@@ -4,7 +4,8 @@
 #else
     #include "RCSim.h"
     const int INT_EDGE_BOTH = 0;
-    static const int digitalRead(int pin) {return 0;}
+    const int INT_EDGE_RISING = 1;
+    static const int digitalRead(int pin) {return 1;}
     static const int wiringPiISR (int pin, int edgeType,  void (*function)(void)) {return 0;}
 #endif
 
@@ -17,35 +18,12 @@
 #include "FirebaseNotification.h"
 #include "Core.h"
 
-
-static std::atomic<bool> call;
-static const int PIN = 22;
-static const int LOW_VALUE = 1;
-static void callback() {
-    unsigned int sleep = 10000;
-    int r = digitalRead(PIN);
-    if(r == LOW_VALUE) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(sleep));
-        r = digitalRead(PIN);
-        if(r == LOW_VALUE) {
-            for(string token : Core::tokenList) {
-                std::cout << token << std::endl;
-                FirebaseNotification* notification = new FirebaseNotification();
-                notification->setTitle("CORRENTE ASSENTE!");
-                notification->setBody("Rilevata assenza di corrente presso la centralina.");
-                notification->setToken(token);
-                notification->setTTL("18000");
-                notification->setNType("battery");
-                FirebaseMessagesHandler::addMessage(notification);
-                Logger::log("Power loss detected");
-                statical::newFirebaseNotification.notify_all();
-            }
-        }
-    }
-}
-
 class PowerLoss {
     private:
+        static const int PIN = 22;
+        static const int LOW_VALUE = 1;
+        static void callback();
+        static void sendNotifications();
     public:
         PowerLoss(){};
         static void startService();
--- source/PowerLoss.cpp
+++ source/PowerLoss.cpp
@@ -1,6 +1,38 @@
 #include "PowerLoss.h"
 void PowerLoss::startService() {
-    call = false;
     wiringPiSetup();
-    wiringPiISR(PIN, INT_EDGE_BOTH, callback);
+    wiringPiISR(PIN, INT_EDGE_RISING, PowerLoss::callback);
+}
+
+void PowerLoss::callback() {
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    int samplingTime = 10;
+    int pwr_loss = 0, pwr_stable = 0;
+    double pwr_loss_thr = 0.75;
+    while(std::chrono::duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now() - begin).count() < samplingTime) {
+        int r = digitalRead(PIN);
+        if(r == LOW_VALUE)
+            pwr_loss++;
+        else
+            pwr_stable++;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+    if((double) pwr_loss / (pwr_loss + pwr_stable) >= pwr_loss_thr) {
+        sendNotifications();
+    }
+}
+
+void PowerLoss::sendNotifications() {
+    for(string token : Core::tokenList) {
+            std::cout << token << std::endl;
+            FirebaseNotification* notification = new FirebaseNotification();
+            notification->setTitle("CORRENTE ASSENTE!");
+            notification->setBody("Rilevata assenza di corrente presso la centralina.");
+            notification->setToken(token);
+            notification->setTTL("18000");
+            notification->setNType("battery");
+            FirebaseMessagesHandler::addMessage(notification);
+            statical::newFirebaseNotification.notify_all();
+    }
+    Logger::log("Power loss detected");
 }
