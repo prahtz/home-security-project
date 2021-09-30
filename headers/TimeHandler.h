8f30f852f3f59553175bcda659f0c44cf6342a21
Prahtz <lorenzopratesi3@gmail.com>
2021-09-30 09:11:23 +0200

    Add expiring time to transmission of codes

--- headers/TimeHandler.h
+++ headers/TimeHandler.h
@@ -13,6 +13,8 @@ struct date {
     int sec;
 };
 
+
+
 class TimeHandler {
     private:
         static void incrementDate(tm* tm);
@@ -20,6 +22,9 @@ class TimeHandler {
         static bool is30dayMonth(int mm);
         static bool isDayIncrementable(int dd, int mm, int yy);
     public:
+        inline static std::chrono::time_point<std::chrono::steady_clock> start;
         static date getDate();
         static void sleepUntillNewDay();
+        static void startTimer();
+        static int elapsedSeconds();
 };
